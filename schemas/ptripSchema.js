b734cb6b5a47a94ea73eef83119764bc2efc77bc
kerokero014 <kevinjrm.kr@gmail.com>
2024-03-30 15:54:49 -0600

    Add basic server setup

---
+++ schemas/ptripSchema.js
@@ -0,0 +1,33 @@
+//SChema for Planning trips using mongoose
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PlanTripSchema = new Schema({
+  tripname: {
+    type: String,
+    required: true
+  },
+  tripdate: {
+    type: Date,
+    required: true
+  },
+  triplocation: {
+    type: String,
+    required: true
+  },
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: true
+  },
+  tripdescription: {
+    type: String,
+    required: true
+  },
+  tripimage: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('PlanTrip', PlanTripSchema);
