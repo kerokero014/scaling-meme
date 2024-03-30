a4888c7445e49f26212d36e2a930f2f87cf8c7bb
kerokero014 <kevinjrm.kr@gmail.com>
2024-03-30 16:29:58 -0600

    Refactor app.js and userSchema.js

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 require('dotenv').config();
-app.use(cors());
-
 // MongoDB connection
 mongoose
   .connect(process.env.MONGODB_URI, {
@@ -25,7 +23,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.use(cors());
 app.use(express.json());
+
+
 app.use('/', routes);
 
 app.listen(port, () => {
--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -19,11 +19,6 @@ const userSchema = new Schema({
     trim: true,
     lowercase: true,
     unique: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error('Email is invalid');
-      }
-    }
   },
   password: {
     type: String,
